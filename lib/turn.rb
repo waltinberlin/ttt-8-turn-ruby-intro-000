779ee6e1f5fae4156d7d3e1702cf4e89190c4811
Walter Minehuber <minehuber@gmail.com>
2017-09-24 14:10:36 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,13 @@
 def turn
 end
 
-def display_board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
 end
 
 def valid_move?
